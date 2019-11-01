6fac724187845e4e78bed83fe59b142e0f41a324
Oliver Zernikow <zernikowoliver@gmail.com>
2019-11-02 00:55:25 +0100

    v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.2.0
+
+* Adding code example for iOS
+* Refactor plugin call
+
 ## 0.1.2
 
 * Fixing missing dependency
--- test/flutter_plugin_bbd_application_test.dart
+++ test/flutter_plugin_bbd_application_test.dart
@@ -16,6 +16,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterPluginBbdApplication.platformVersion, '42');
+    expect(await FlutterPluginBbdApplication.getVersion, '42');
   });
 }
